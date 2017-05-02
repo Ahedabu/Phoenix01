0ecb3f318cd8e2b983b2d7407006b95adc96dc2f
lytwaytLaz <laszlo.hago@gmail.com>
2017-05-02 15:33:43 +0200

    Clean up project, delete files and folders

--- src/Phoenix01/wwwroot/_references.js
+++ src/Phoenix01/wwwroot/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/ajaxtest.js" />
 /// <reference path="js/querytest.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />

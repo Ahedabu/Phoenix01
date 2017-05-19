ec8ba03a4fc84ed9b642b5020eba86444358bc50
MALiK <MALiK@LAPTOP-156ULR1Q>
2017-05-19 10:32:57 +0200

    Minor changes

--- src/Phoenix01/Controllers/AccountController.cs
+++ src/Phoenix01/Controllers/AccountController.cs
@@ -122,7 +122,7 @@ namespace Phoenix01.Controllers
                     //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
-                    return RedirectToAction(nameof(ManageController.UserProfile), "UserProfile");
+                    return RedirectToAction(nameof(ManageController.UserProfile), "Manage");
                 }
                 AddErrors(result);
             }

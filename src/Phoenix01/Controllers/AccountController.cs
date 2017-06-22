fa4ca387c24fbe6dab449187fd6025b272837ccf
lytwaytLaz <laszlo.hago@gmail.com>
2017-06-22 14:29:41 +0200

    Remove unneccessary validation

--- src/Phoenix01/Controllers/AccountController.cs
+++ src/Phoenix01/Controllers/AccountController.cs
@@ -57,28 +57,6 @@ namespace Phoenix01.Controllers
         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
-            if (model.Email.IndexOf('@') > -1)
-            {
-                //Validate email format
-                string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
-                                       @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                                          @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
-                Regex re = new Regex(emailRegex);
-                if (!re.IsMatch(model.Email))
-                {
-                    ModelState.AddModelError("Email", "Email is not valid");
-                }
-            }
-            else
-            {
-                //validate Username format
-                string emailRegex = @"^[a-zA-Z0-9]*$";
-                Regex re = new Regex(emailRegex);
-                if (!re.IsMatch(model.Email))
-                {
-                    ModelState.AddModelError("Email", "Username is not valid");
-                }
-            }
 
             if (ModelState.IsValid)
             {
